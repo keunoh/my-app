25fb47a2e5eacf5e9c9183b80cef24c27073ff79
keunoh <rmsdhcjswo1@gmail.com>
2025-01-04 23:17:06 +0900

    HTML to JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import Gallery from "./Gallery";
-import { Profile } from "./Gallery.js"
 
-export default function App() {
+export default function TodoList() {
   return (
-    <Profile />
+    <div>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </div>
   );
 }
